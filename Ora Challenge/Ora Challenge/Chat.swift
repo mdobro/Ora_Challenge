4939f560399fe025534ea644bd12ff32533f0767
Mike Dobrowolski <mdobro@umich.edu>
2017-09-18 14:25:09 -0400

    added Alamofire via Cocoapods

---
+++ Ora Challenge/Ora Challenge/Chat.swift
@@ -0,0 +1,13 @@
+//
+//  Chat.swift
+//  Ora Challenge
+//
+//  Created by Mike Dobrowolski on 9/18/17.
+//  Copyright © 2017 Ora. All rights reserved.
+//
+
+import Foundation
+
+class Chat {
+    
+}
--- Ora Challenge/Ora Challenge/ViewController.swift
+++ Ora Challenge/Ora Challenge/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Table Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 }
 
